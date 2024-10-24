801f309cb246fa78a700ca275b1508a2194d2b52
garrydu <garrydu17sep2021@gmail.com>
2024-10-24 14:18:36 -0700

    add AD and p-value

--- code/dist_fit.py
+++ code/dist_fit.py
@@ -5,9 +5,11 @@ from math import log as ln
 from math import exp
 from numpy import nan
 import statistics as stat
-#  import numpy as np
+import numpy as np
 from prettytable import PrettyTable as PT
 #  from scipy.optimize import minimize
+##### Own Modules #####
+from utilities import norm_test
 
 
 def ABL(rv, data, k):
@@ -30,7 +32,7 @@ def ABL(rv, data, k):
     return (-2 * log_likelihood, AICc, BIC)
 
 
-def print_table(fit_name, para_names, paras, abl, print_port=print):
+def print_table(fit_name, para_names, paras, abl, a2, p, print_port=print):
     print = print_port
     print("\n---- Fitted " + fit_name + " Distribution ----")
     t = PT()
@@ -42,7 +44,60 @@ def print_table(fit_name, para_names, paras, abl, print_port=print):
     t.field_names = ['Measures', '-2*LogLikelihood', 'AICc', 'BIC']
     t.add_row(["Value", "%.2f" % abl[0], "%.2f" % abl[1], "%.2f" % abl[2]])
     print(str(t))
+    t = PT()
+    t.field_names = [' ', 'A-sq', 'Approx. p-value']
+    t.add_row(["Anderson-Darling", "%.2f" % a2, "%.3f" % p])
+    print(str(t))
+    print("H0: The data is from the distribution. Small p-value rejects H0.")
     return
+
+
+def AD(rv, data):
+    n = len(data)
+    data = np.array(data, dtype=float)
+    data = np.sort(data)
+    cdf = rv.cdf(data)
+# Calculate the AD statistic
+    ad_stat = -n - np.sum((2 * np.arange(1, n + 1) - 1) * (np.log(cdf) + np.log(1 - cdf[::-1]))) / n
+    return ad_stat
+
+
+def p_simulation(rv, a2, n, TTL=10000):
+    cnt = 0
+    for i in range(TTL):
+        if AD(rv, rv.rvs(size=n)) >= a2:
+            cnt += 1
+    return cnt / TTL
+
+
+def approximate_p(ad_adjusted):
+    # The approximate p-value for the second test statistic is computed from a set of four interpolating functions
+    # R.B. D'Augostino and M.A. Stephens, Eds., 1986, Goodness-of-Fit Techniques, Marcel Dekker.
+
+    #  ad_adjusted = ad_stat * (1 + (0.75/n) + (2.25/n**2))
+    # However, I found without adj the p value is closer to the JMP simulated p-value
+
+    if ad_adjusted >= 0.6:
+        p = np.exp(1.2937 - 5.709 * ad_adjusted + 0.0186 * ad_adjusted**2)
+    elif 0.34 < ad_adjusted < 0.6:
+        p = np.exp(0.9177 - 4.279 * ad_adjusted - 1.38 * ad_adjusted**2)
+    elif 0.2 < ad_adjusted <= 0.34:
+        p = 1 - np.exp(-8.318 + 42.796 * ad_adjusted - 59.938 * ad_adjusted**2)
+    else:  # ad_adjusted <= 0.2
+        p = 1 - np.exp(-13.436 + 101.14 * ad_adjusted - 223.73 * ad_adjusted**2)
+
+    return p
+
+
+def goodness_of_fit(rv, data, normal=False):
+    if normal:
+        res = norm_test(data, print_out=False)
+        return res['AD s'], res['AD p']
+    else:
+        a2 = AD(rv, data)
+        #  p = p_simulation(rv, a2, len(data))
+        p = approximate_p(a2)
+    return a2, p
 
 
 def fit_norm(data, print_out=True, print_port=print):
@@ -52,9 +107,10 @@ def fit_norm(data, print_out=True, print_port=print):
     rv = norm(u, s)
     #  n = len(data)
     abl = ABL(rv, data, 2)
+    a2, p = goodness_of_fit(rv, data, normal=True)
     if print_out:
         print_table("Normal", ("Location mu", "Dispersion sigma"),
-                    (u, s), abl, print_port=print_port)
+                    (u, s), abl, a2, p, print_port=print_port)
     return {"fits": (u, s), "ABL": abl, "rv": rv}
 
     #  sem = s / n**.5
@@ -73,10 +129,11 @@ def fit_t(data, print_out=True, print_port=print):
     #  print(ABL(t_dist(df), (data - u) / s, 3))
     rv = t_dist(df, loc=u, scale=s)
     abl = ABL(rv, data, 3)
+    a2, p = goodness_of_fit(rv, data)
     if print_out:
         print_table("Stundent's t",
                     ("Location mu", "Scale sigma", "DF"),
-                    (u, s, df), abl, print_port=print_port)
+                    (u, s, df), abl, a2, p, print_port=print_port)
     return {"fits": (u, s, df), "ABL": abl, "rv": rv}
 
     ##### The minimum fitting gives similar but not same results #####
@@ -112,9 +169,10 @@ def fit_gamma(data, print_out=True, print_port=print):
     #  print(ABL(t_dist(df), (data - u) / s, 3))
     rv = gamma(u, loc=0, scale=s)
     abl = ABL(rv, data, 2)
+    a2, p = goodness_of_fit(rv, data)
     if print_out:
         print_table("Gamma", ("Shape alpha", "Scale sigma"),
-                    (u, s), abl, print_port=print_port)
+                    (u, s), abl, a2, p, print_port=print_port)
     return {"fits": (u, s), "ABL": abl, "rv": rv}
 
 
@@ -124,9 +182,10 @@ def fit_weibull(data,
     u, _, s = weibull_min.fit(data, floc=0)
     rv = weibull_min(u, loc=0, scale=s)
     abl = ABL(rv, data, 2)
+    a2, p = goodness_of_fit(rv, data)
     if print_out:
         print_table("Weibull", ("Scale alpha", "Shape beta"),
-                    (s, u), abl, print_port=print_port)
+                    (s, u), abl, a2, p, print_port=print_port)
     return {"fits": (s, u), "ABL": abl, "rv": rv}
 
 
@@ -137,9 +196,10 @@ def fit_lognorm(data,
     #  print(a, ln(s))
     rv = lognorm(a, loc=0, scale=s)
     abl = ABL(rv, data, 2)
+    a2, p = goodness_of_fit(rv, data)
     if print_out:
         print_table("Lognormal", ("Scale mu", "Shape sigma"),
-                    (ln(s), a), abl, print_port=print_port)
+                    (ln(s), a), abl, a2, p, print_port=print_port)
     return {"fits": (s, a), "ABL": abl, "rv": rv}
 
 
@@ -153,9 +213,10 @@ def fit_expon(data,
     _, s = expon.fit(data, floc=0)
     rv = expon(loc=0, scale=s)
     abl = ABL(rv, data, 1)
+    a2, p = goodness_of_fit(rv, data)
     if print_out:
         print_table("Exponential", ("Scale sigma", ),
-                    (s, ), abl, print_port=print_port)
+                    (s, ), abl, a2, p, print_port=print_port)
     return {"fits": (s,), "ABL": abl, "rv": rv}
 
 
@@ -251,6 +312,7 @@ if __name__ == "__main__":
         62,
         68,
         70]
+    data = [1, 2, 3, 4, 2, 12, 3, 12, 3, 2, 14, 3, 2]
     fit_norm(data)
     fit_t(data)
     fit_gamma(data)
